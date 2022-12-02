a68595d7705aba71d9d2fbea3df015d62272fca6
Munkh-Erdene12 <58659319+Munkh-Erdene12@users.noreply.github.com>
2022-12-02 17:37:31 +0800

    Update main.js

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ try {
     const sections = document.querySelectorAll(".section");
     sections.forEach((section) => {
       if (!section.classList.contains("active")) {
-        section.classList.add("hide");
+        // section.classList.add("hide");
       }
     });
   })();
